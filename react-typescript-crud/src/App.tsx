d2c1e9f09a39983c61b2835c79408c2fac93ab13
unknown <yuniorangeles06@gmail.com>
2023-08-19 00:01:40 +0200

    separte by components

--- react-typescript-crud/src/App.tsx
+++ react-typescript-crud/src/App.tsx
@@ -1,61 +1,20 @@
-import { useState, useEffect} from 'react'
+import ViewDataForm from './components/ViewDataForm'
+import Form from './components/form'
 import './App.css'
 
 
-interface Formdata {
 
-  name: string
-  email: string
-  password: string
-}
 
 function App() {
 
-  const [data, setData] = useState<Formdata[]>([])
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const formData = new FormData(e.currentTarget)
-    
-    const newData: Formdata = {
-      name: formData.get('name') as string,
-      email: formData.get('email') as string,
-      password: formData.get('password') as string,
-    }
-    console.log(newData)
-    setData([...data, newData])
-  }
-
-useEffect(() => {
-  data.map((item) => {
-    return item
-  })}),[data]
 
   return (
     <>
-<form onSubmit={handleSubmit}>
+    <Form />
+    <ViewDataForm />
 
-  <label htmlFor="">name</label>
-  <input type="text" name="name" id="name" />
-  <label htmlFor="">email</label>
-  <input type="email" name="email" id="email" />
-  <label htmlFor="">password</label>
-  <input type="password" name="password" id="password" />
-  <input type="submit" value="enviar" />
-</form>
-       
-<div className="div-data-container">
-  {data.map((item) => {
-    return(
-      <div className="div-data">
-        <p className="nameclass">{item.name}</p>
-        <p className='emailclass'>{item.email}</p>
-        <p className='passwordclass'>{item.password}</p>
-      </div>
-    )
-  })}
+
   
-    </div>
      
     </>
   )
