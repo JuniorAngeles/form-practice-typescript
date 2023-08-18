2469f75748528e0e81821ce0b1ef0e322f0ef2bb
unknown <yuniorangeles06@gmail.com>
2023-08-18 23:59:16 +0200

    implement login logic

---
+++ react-typescript-crud/src/components/login.tsx
@@ -0,0 +1,18 @@
+
+
+export default function Login (){
+
+
+    return(
+     <>
+        <form action="">
+            <label htmlFor="">email</label>
+            <input type="email" name="email" id="email" />
+            <label htmlFor="">password</label>
+            <input type="password" name="password"/>
+        </form>
+
+     </>
+    )
+
+}
